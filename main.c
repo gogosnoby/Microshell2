df5ca61a98829d5dc629f16e6c8ee425eaf514f8
gogosnoby <oskarwiniarski09@gmail.com>
2022-12-18 11:03:47 -0500

    Dzielenie userinput na komendy/dodanie funkcjonalnosci help i exit
    dzieli userinput na poszczegolne komendy przy pomocy strtok. obsluguje proste komendy help oraz exit wyswietlajac komunikat bledu przy dodatkowym argumencie

--- main.c
+++ main.c
@@ -16,10 +16,34 @@
 
 #define BUFFER_SIZE 2048
 
+void help()
+{
+    printf(KRED "\n===Microshell===\n\n" RESET);
+    printf(KRED "Autor: Oskar Winiarski\n\n" RESET);
+    printf(KRED "Obslugiwane komendy: \n-help\n-exit" RESET);
+    printf("\n\n");
+}
+
+void argument(char *command, char **arg)
+{
+    char korektor[]=" \n";
+    char *com=strtok(command,korektor);
+    int i=0;
+    while(com!=NULL)
+    {
+        arg[i]=com;
+        i++;
+        com=strtok(NULL,korektor);
+    }
+    arg[i]=0;
+}
+
 int main()
 {
-    char polecenie[100];
+    char uinput[BUFFER_SIZE];
     char cwd[BUFFER_SIZE];
+    char *arg[100];
+    char *command;
     while(true)
     {
         char *login=getlogin();
@@ -29,11 +53,27 @@ int main()
         printf(KBLU "[%s]" RESET, cwd);
         printf("$ ");
 
-        fgets(polecenie, sizeof(polecenie), stdin);
-        polecenie[strlen(polecenie)-1]=0;
+        command=fgets(uinput, sizeof(uinput), stdin);
+        uinput[strlen(uinput)-1]='\0';
+        argument(command, arg);
+
+        if(strcmp(arg[0],"help")==0)
+        {
+            if(arg[1]==NULL)
+                help();
+            else
+                printf("Wrong argument for command %s\n", arg[0]);
+        }
+
+        else if(strcmp(arg[0],"exit")==0)
+        {
+            if(arg[1]==NULL)
+                exit(0);
+            else
+                printf("Wrong argument for command %s\n", arg[0]);
+        }
 
 
-        return 0;
     }
     return 0;
 }
